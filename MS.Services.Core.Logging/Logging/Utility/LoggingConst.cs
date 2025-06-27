a498497ff25db070f0e62492fb0079f3ede07097
Kemal Çetin <73641701+KemalCetin01@users.noreply.github.com>
2025-06-28 00:06:03 +0300

    first

---
+++ MS.Services.Core.Logging/Logging/Utility/LoggingConst.cs
@@ -0,0 +1,7 @@
+﻿namespace MS.Services.Core.Logging.Logging.Utility;
+
+public static class LoggingConst
+{
+    public const string CorrelationId = "OzCorrelationID";
+    public const string LogType = "LogType";
+}
---
+++ MS.Services.Core.Logging/ServiceRegistration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MS.Services.Core.Logging.Enrich;
+using Serilog;
+using Serilog.Formatting.Elasticsearch;
+
+namespace MS.Services.Core.Logging;
+
+public static class ServiceRegistration
+{
+    public static WebApplicationBuilder UseSerilogLogging(this WebApplicationBuilder builder,IConfiguration configuration)
+    {
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddSingleton<CorrelationIdEnricher>();
+
+        builder.Logging.ClearProviders();
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")!;
+
+
+        builder.Host.UseSerilog((_, serviceProvider, config) =>
+        {
+            var httpContext = serviceProvider.GetRequiredService<CorrelationIdEnricher>();
+
+            config
+                .Enrich.With(httpContext)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("environment", environment)
+                .Enrich.WithCorrelationIdHeader("CorrelationId")
+                .WriteTo.Console(new ElasticsearchJsonFormatter())
+                .ReadFrom.Configuration(configuration);
+        });
+        return builder;
+    }
+}
